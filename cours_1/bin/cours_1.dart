d1ed7deccead74ebd9a457ac2afdcfdf5f7ee6ee
Kairouz Alexia <alexia.kairouz@edu.ece.fr>
2026-01-16 12:39:54 +0100

    tpcours1

---
+++ cours_1/bin/cours_1.dart
@@ -0,0 +1,5 @@
+import 'package:cours_1/cours_1.dart' as cours_1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${cours_1.calculate()}!');
+}
---
+++ cours_1/test/cours_1_test.dart
@@ -0,0 +1,8 @@
+import 'package:cours_1/cours_1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
